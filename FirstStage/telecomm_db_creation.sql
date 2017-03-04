31478539b866dc81cd04d7ebcf7bdb27bd9fed91
Mandzhiev Ayta <mandzhiev.ayta@mail.ru>
2017-03-04 16:58:11 +0300

    First stage

---
+++ FirstStage/telecomm_db_creation.sql
@@ -0,0 +1,127 @@
+CREATE TABLE `Clients` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`type` enum('individual', 'organization') NOT NULL,
+	PRIMARY KEY (`id`, `type`)
+);
+
+CREATE TABLE `Individuals` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`client_id` int NOT NULL,
+	`first_name` varchar(100),
+	`second_name` varchar(100),
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Organizations` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`client_id` int NOT NULL,
+	`name` varchar(100) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Contacts` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`client_id` int NOT NULL,
+	`first_name` varchar(100) NOT NULL,
+	`second_name` varchar(100) NOT NULL,
+	`address` varchar(100),
+	`phone_number` varchar(100),
+	`mail` varchar(100),
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Accounts` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`client_id` int NOT NULL,
+	`balance` int NOT NULL,
+	`max_debt` int NOT NULL,
+	`pay_debt_date` DATE NOT NULL,
+	`pay_debt_time` TIME NOT NULL,
+	`phone_number` varchar(100) NOT NULL UNIQUE,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `NumberGroups` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`group_number` int NOT NULL,
+	`account_id` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `ReplenishHistory` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`account_id` int NOT NULL,
+	`date` DATE NOT NULL,
+	`time` TIME NOT NULL,
+	`amount` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `ContractsHistory` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`account_id` int NOT NULL,
+    `relevance` enum('active', 'ended') NOT NULL,
+	`tariff_type` enum('call', 'sms', 'internet') NOT NULL,
+	`calltariff_id` int,
+	`smstariff_id` int NOT NULL,
+	`internettariff_id` int NOT NULL,
+	`date` DATE NOT NULL,
+	`time` TIME NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `WriteoffsHistory` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `contract_id` int NOT NULL,
+    `account_id` int NOT NULL,
+	`date` DATE NOT NULL,
+	`time` TIME NOT NULL,
+    `value` int NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `SmsTariffs` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`name` varchar(100) NOT NULL,
+	`price` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `CallTariffs` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`price` int NOT NULL,
+	`name` varchar(100) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `InternetTariffs` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`name` varchar(100) NOT NULL,
+	`price` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `Contacts` ADD CONSTRAINT `FK_Contacts_client` FOREIGN KEY (`client_id`) REFERENCES `Clients`(`id`);
+
+ALTER TABLE `Individuals` ADD CONSTRAINT `FK_Individuals_client` FOREIGN KEY (`client_id`) REFERENCES `Clients`(`id`);
+
+ALTER TABLE `ContractsHistory` ADD CONSTRAINT `FK_ContractsHistory_client` FOREIGN KEY (`calltariff_id`) REFERENCES `CallTariffs`(`id`);
+
+ALTER TABLE `ContractsHistory` ADD CONSTRAINT `FK_ContractsHistory_smstariff` FOREIGN KEY (`smstariff_id`) REFERENCES `SmsTariffs`(`id`);
+
+ALTER TABLE `ContractsHistory` ADD CONSTRAINT `FK_ContractsHistory_internettariff` FOREIGN KEY (`internettariff_id`) REFERENCES `InternetTariffs`(`id`);
+
+ALTER TABLE `ContractsHistory` ADD CONSTRAINT `FK_ContractsHistory_account` FOREIGN KEY (`account_id`) REFERENCES `Accounts`(`id`);
+
+ALTER TABLE `WriteoffsHistory` ADD CONSTRAINT `FK_WriteoffsHistory_account` FOREIGN KEY (`account_id`) REFERENCES `Accounts`(`id`);
+
+ALTER TABLE `WriteoffsHistory` ADD CONSTRAINT `FK_WriteoffsHistory_contract` FOREIGN KEY (`contract_id`) REFERENCES `ContractsHistory`(`id`);
+
+ALTER TABLE `Organizations` ADD CONSTRAINT `FK_Organizations_client` FOREIGN KEY (`client_id`) REFERENCES `Clients`(`id`);
+
+ALTER TABLE `ReplenishHistory` ADD CONSTRAINT `FK_ReplenishHistory_account` FOREIGN KEY (`account_id`) REFERENCES `Accounts`(`id`);
+
+ALTER TABLE `Accounts` ADD CONSTRAINT `FK_Accounts_account` FOREIGN KEY (`client_id`) REFERENCES `Clients`(`id`);
+
+ALTER TABLE `NumberGroups` ADD CONSTRAINT `FK_NumberGroups_account` FOREIGN KEY (`account_id`) REFERENCES `Accounts`(`id`);
+
---
+++ FirstStage/telecomm_db_drop.sql
@@ -0,0 +1,48 @@
+ALTER TABLE `Contacts` DROP FOREIGN KEY `FK_Contacts_client`;
+
+ALTER TABLE `Individuals` DROP FOREIGN KEY `FK_Individuals_client`;
+
+ALTER TABLE `ContractsHistory` DROP FOREIGN KEY `FK_ContractsHistory_client`;
+
+ALTER TABLE `ContractsHistory` DROP FOREIGN KEY `FK_ContractsHistory_smstariff`;
+
+ALTER TABLE `ContractsHistory` DROP FOREIGN KEY `FK_ContractsHistory_internettariff`;
+
+ALTER TABLE `ContractsHistory` DROP FOREIGN KEY `FK_ContractsHistory_account`;
+
+ALTER TABLE `WriteoffsHistory` DROP FOREIGN KEY `FK_WriteoffsHistory_contract`;
+
+ALTER TABLE `WriteoffsHistory` DROP FOREIGN KEY `FK_WriteoffsHistory_account`;
+
+ALTER TABLE `Organizations` DROP FOREIGN KEY `FK_Organizations_client`;
+
+ALTER TABLE `ReplenishHistory` DROP FOREIGN KEY `FK_ReplenishHistory_account`;
+
+ALTER TABLE `Accounts` DROP FOREIGN KEY `FK_Accounts_account`;
+
+ALTER TABLE `NumberGroups` DROP FOREIGN KEY `FK_NumberGroups_account`;
+
+DROP TABLE IF EXISTS `Contacts`;
+
+DROP TABLE IF EXISTS `Individuals`;
+
+DROP TABLE IF EXISTS `WriteoffsHistory`;
+
+DROP TABLE IF EXISTS `ContractsHistory`;
+
+DROP TABLE IF EXISTS `SmsTariffs`;
+
+DROP TABLE IF EXISTS `Organizations`;
+
+DROP TABLE IF EXISTS `ReplenishHistory`;
+
+DROP TABLE IF EXISTS `Accounts`;
+
+DROP TABLE IF EXISTS `CallTariffs`;
+
+DROP TABLE IF EXISTS `InternetTariffs`;
+
+DROP TABLE IF EXISTS `Clients`;
+
+DROP TABLE IF EXISTS `NumberGroups`;
+
